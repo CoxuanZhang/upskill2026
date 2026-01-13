0dcfb77b8f0cf619b5a3f020c2d7e7f0d83eb68c
“CoxuanZhang” <cocozhangkx0307@gmail.com>
2026-01-13 10:42:58 -0500

    "user input fav fruit"

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 fruits = ['apple', 'banana', 'orange', 'grape', 'mango']
 for fruit in fruits:
     print(fruit)
+
+user = input("What is your fav fruit?")
+
+if user in fruits:
+    print("Great choice!")
+else:
+    print("Nice, I haven't tried that one.")
